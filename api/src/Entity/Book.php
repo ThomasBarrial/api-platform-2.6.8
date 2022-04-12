89499792b2e779f44fb49b31333d186a70a5365e
Thomas Barrial <thomas.barrial@gmail.com>
2022-04-12 22:23:46 +0200

    First try to create entities with relations and full CRUD API

---
+++ api/src/Entity/Book.php
@@ -0,0 +1,79 @@
+<?php
+// api/src/Entity/Book.php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/** A book. */
+#[ORM\Entity]
+#[ApiResource(
+    routePrefix: '/library',
+    collectionOperations: ['get', 'post'],
+    itemOperations: ['get', 'put', 'delete'],
+)]
+class Book
+{
+    /** The id of this book. */
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    private int $id;
+
+    /** The ISBN of this book (or null if doesn't have one). */
+    #[ORM\Column(nullable: true)]
+    #[Assert\Isbn]
+    public ?string $isbn = null;
+
+    /** The title of this book. */
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    public string $title = '';
+
+    /** The description of this book. */
+    #[ORM\Column(type: "text")]
+    #[Assert\NotBlank]
+    public string $description = '';
+
+    /** The author of this book. */
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    public string $author = '';
+
+    /** The publication date of this book. */
+    #[ORM\Column(type: "datetime")]
+    #[Assert\NotNull]
+    public ?\DateTimeInterface $publicationDate = null;
+
+    /** @var Review[] Available reviews for this book. */
+    #[ORM\OneToMany(mappedBy: 'book', targetEntity: 'Review', cascade: ['persist', 'remove'])]
+    public iterable $reviews;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'books')]
+    private $usersbook;
+
+    public function __construct()
+    {
+        $this->reviews = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsersbook(): ?User
+    {
+        return $this->usersbook;
+    }
+
+    public function setUsersbook(?User $usersbook): self
+    {
+        $this->usersbook = $usersbook;
+
+        return $this;
+    }
+}
+
+?>
---
+++ api/src/Entity/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ApiResource]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $email;
+
+    #[ORM\OneToMany(mappedBy: 'usersbook', targetEntity: Book::class)]
+    private $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Book[]
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): self
+    {
+        if (!$this->books->contains($book)) {
+            $this->books[] = $book;
+            $book->setUsersbook($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): self
+    {
+        if ($this->books->removeElement($book)) {
+            // set the owning side to null (unless already changed)
+            if ($book->getUsersbook() === $this) {
+                $book->setUsersbook(null);
+            }
+        }
+
+        return $this;
+    }
+}
